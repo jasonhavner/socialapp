c2d59d50e01f4f9edc3166aa7b21c346a8a21f61
Jason Havner <jason@iotamediagroup.com>
2013-06-17 21:44:35 -0700

    created rail app and status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialapp::Application.config.secret_token = '97710cedd1fcf5a7d7a8035dbf60287923092627e3e6075999bd73d1a79f86dad1015d841c14c31bd609fc54b8a249ff0cddc7740a695a1cf251c021b14f5cbc'
